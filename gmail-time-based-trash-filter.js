b0ec41b2bed12570d46e1adcf9360cdfe0da8136
Rich Martinez <richmartinez@quickenloans.com>
2017-10-12 10:46:15 -0400

    Optimize script to be more readable and self documenting

--- gmail-time-based-trash-filter.js
+++ gmail-time-based-trash-filter.js
@@ -1,77 +1,64 @@
-/* Edited by Richard Martinez - http://richardmartinez.github.io/ */
-
-//Change the value of labelTemplate to whichever label you choose.
-var labelTemplate = "DELETE/ThisAfterDay";
-
-//change list to match all of your labels day values
-var listOfDays = [1, 5, 7, 14, 30, 60];
-
-//optionally change timezone
-var timeZone = "America/Detroit";
-
-
-
-
-
-var queryObjectConstructor = function(listOfDays) {
-  var arrayOfLabelInfo = [];
-  for (var i = 0, l = listOfDays.length; i < l; i++) {
-    arrayOfLabelInfo.push({
-      "GMAIL_LABEL": labelTemplate + listOfDays[i],
-      "PURGE_AFTER": listOfDays[i]
-    });
+function config() {
+  return {
+    //Change the value of labelTemplate to whichever label you choose.
+    labelTemplate: "DELETE/ThisAfterDay",
+    //change list to match all of your labels day values
+    listOfDays: [1, 5, 7, 14, 30, 60],
+    //optionally change timezone
+    timeZone: "America/Detroit",
+    useTimeZone: timeZone || Session.getScriptTimeZone(),
+    gmailLabelKey: 'gmailLabel',
+    purgeAfterKey: 'purgeAfter'
   }
-  return arrayOfLabelInfo;
-};
+}
 
-var useTimeZone = timeZone || Session.getScriptTimeZone();
+function createQueryObjects(listOfDays, labelTemplate, gmailLabelKey, purgeAfterKey) {
+  var queryObjects = listOfDays.map(function (day) {
+    return {
+      "GMAIL_LABEL": labelTemplate + day,
+      "PURGE_AFTER": day
+    };
+  });
 
-function Intialize() {
+  return queryObjects;
+}
+
+function intialize() {
   return;
 }
 
-function Install() {
-
+function install() {
   ScriptApp.newTrigger("purgeEachLabel")
     .timeBased()
     .everyDays(1)
     .inTimezone(useTimeZone)
     .create();
-
 }
 
-function Uninstall() {
+function uninstall() {
   var triggers = ScriptApp.getProjectTriggers();
   for (var i = 0; i < triggers.length; i++) {
     ScriptApp.deleteTrigger(triggers[i]);
   }
-
 }
 
-function purgeEachLabel() {
-  var currentLabel, currentDay, arrayOfLabelInfo = queryObjectConstructor(listOfDays);
-  for (var i = 0; i < arrayOfLabelInfo.length; i++) {
-    currentLabel = arrayOfLabelInfo[i]["GMAIL_LABEL"];
-    currentDay = arrayOfLabelInfo[i]["PURGE_AFTER"];
-    purgeGmail(currentLabel, currentDay);
-  }
-}
-
-var purgeGmail = function(currentLabel, currentDay) {
-
+function purgeGmail(currentLabel, purgeAfterDayNumber) {
+  /**
+   * TODO: optimize age to be easier to read and more self documenting
+   */
   var age = new Date();
-  age.setDate(age.getDate() - currentDay);
-
-  var query = "label:" + currentLabel + " older_than:" + currentDay + "d";
+  age.setDate(age.getDate() - purgeAfterDayNumber);
+  var query = "label:" + currentLabel + " older_than:" + purgeAfterDayNumber + "d";
 
   try {
-
     var threads = GmailApp.search(query, 0, 100);
-
     if (threads.length == 100) {
-      purgeGmail(currentLabel, currentDay);
+      purgeGmail(currentLabel, purgeAfterDayNumber);
     }
 
+    /**
+     * Attempt to replace for loops with array functions based on what is returned from the Gmail methods
+     */
     for (var i = 0; i < threads.length; i++) {
       var messages = GmailApp.getMessagesForThread(threads[i]);
       for (var j = 0; j < messages.length; j++) {
@@ -81,7 +68,27 @@ var purgeGmail = function(currentLabel, currentDay) {
         }
       }
     }
+  } catch (e) {
+    /**
+     * TODO: Figure out how send emails if an error happens.
+     */
+  }
+}
 
-  } catch (e) {}
+function purgeEachLabel() {
+  var config = config();
+  var currentLabel;
+  var currentDay;
+  var queryObjects = queryObjectConstructor(
+    config.listOfDays,
+    config.labelTemplate,
+    config.gmailLabelKey,
+    config.purgeAfterKey
+  );
 
-};
+  queryObjects.forEach(function(queryObject) {
+    currentLabel = queryObject[config.gmailLabelKey];
+    purgeAfterDayNumber = queryObject[config.purgeAfterKey];
+    purgeGmail(currentLabel, purgeAfterDayNumber);
+  });
+}
